e1685276ddd3b0b6f921f74f82360e8a0e1df784
xinjg <574218911@qq.com>
2015-03-21 23:37:23 +0800

    Prevent this class from being instansed

--- leetCode/src/org/xinjg/util/ArrayUtil.java
+++ leetCode/src/org/xinjg/util/ArrayUtil.java
@@ -1,6 +1,9 @@
 package org.xinjg.util;
 
 public class ArrayUtil<T> {
+	private ArrayUtil(){
+		
+	}
 	public static<T> void printArray(int[] num){
 		System.out.print( "  [  ");
 		for( int i=0;i<num.length;i++ ){
@@ -8,6 +11,16 @@ public class ArrayUtil<T> {
 				System.out.print( num[i] +"  ]  ");
 			else
 				System.out.print( num[i] +"  ,  ");
+		}
+	}
+	
+	public static void printAsChar(int[] num){
+		System.out.print( "  [  ");
+		for( int i=0;i<num.length;i++ ){
+			if( i==num.length-1 )
+				System.out.print( (char)num[i] +"  ]  ");
+			else
+				System.out.print( (char)num[i] +"  ,  ");
 		}
 	}
 }
