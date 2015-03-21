9b5a24e170b0307e9627123b220edd88242c0e05
xinjg <574218911@qq.com>
2015-03-21 11:29:46 +0800

    Add Simple Top Down Merge Sort

--- leetCode/src/org/xinjg/sort/SortUtil.java
+++ leetCode/src/org/xinjg/sort/SortUtil.java
@@ -3,6 +3,51 @@ package org.xinjg.sort;
 import org.xinjg.util.ArrayUtil;
 
 public class SortUtil {
+	
+	/**
+	 * Top down merge sort 
+	 * @param a
+	 */
+	public static void mergeSort(int[] a){
+		int[] aux = new int[a.length];
+		sort(a,aux,0,a.length-1);
+	}
+	
+	/**
+	 * 
+	 */
+	public static void sort(int[] a ,int aux[],int lo,int hi) {
+		if (lo>=hi)
+			return;
+		int mid = lo + (hi-lo)/2;
+		sort( a,aux,lo,mid );
+		sort( a,aux,mid+1,hi );
+		merge( a,aux,lo,mid,hi );
+	}
+	
+	/**
+	 *  merge sorted arrays  a[lo]~a[mid]  and a[mid+1]~a[hi]
+	 * @param a
+	 * @param aux
+	 * @param lo
+	 * @param mid
+	 * @param hi
+	 */
+	public static void merge( int[] a,int[] aux,int lo,int mid,int hi ){
+		for(int i=lo;i<=hi;i++){
+			aux[i]=a[i];
+		}
+		for(int k=lo,i=lo,j=mid+1;i<=mid||j<=hi ;k++){
+			if( i>mid )
+				a[k]=aux[j++];
+			else if(j>hi)
+				a[k]=aux[i++];
+			else if(aux[i] <aux[j])
+				a[k]=aux[i++];
+			else 
+				a[k]=aux[j++];
+		}
+	}
 
 	/**
 	 * Simple Implementation of Selection Sort 
@@ -43,8 +88,8 @@ public class SortUtil {
 	}
 	
 	public static void main(String[] args) {
-		int[] a ={-1, 0 ,1, 2, -1 ,-4};
-		insertionSort(a );
+		int[] a ={4, 5 ,6, 1, 2 ,3};
+		mergeSort(a);
 		ArrayUtil.printArray(a);
 	}
 	
