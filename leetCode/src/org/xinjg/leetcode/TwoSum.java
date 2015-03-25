b9fd7bb5bd17aa17e374db999a45d6565d663dad
xinjg <574218911@qq.com>
2015-03-25 16:54:49 +0800

    With Simple Sorted Table  not Accepted

--- leetCode/src/org/xinjg/leetcode/TwoSum.java
+++ leetCode/src/org/xinjg/leetcode/TwoSum.java
@@ -1,5 +1,7 @@
 package org.xinjg.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 
  * Given an array of integers, find two numbers such that they add up to a
@@ -15,17 +17,94 @@ package org.xinjg.leetcode;
  * 
  */
 public class TwoSum {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] ret = null;
-        int len = numbers.length;
-        for(int i=0;i<len;i++ ){
-        	for( int j =i+1;j<len;j++ ){
-        		if( numbers[i]+numbers[j] == 9 ){
-        			ret = new int[2];ret[0]=i;ret[1]=j;
-        			return ret;
-        		}
-        	}
-        }
-        return ret;
-    }
+
+	//Simple Sorted Table 
+	private class SortedTable {
+		// Constructor
+		SortedTable(int capacity) {
+			this.capacity = capacity;
+			this.keys = new int[capacity];
+			this.values = new int[capacity];
+		}
+
+		public void put(int key, int value) {
+
+			int pos = findPos(keys, key, 0, cnt - 1);
+			// check equal
+			if (keys[pos] == key) {
+				values[pos] = value;
+				return;
+			}
+
+			int idx = cnt - 1;
+			// remove all element greater than key
+			while (idx >= pos) {
+				swap(keys, idx, idx + 1);
+				swap(values, idx, idx + 1);
+				idx--;
+			}
+			keys[pos] = key;
+			values[pos] = value;
+			cnt++;
+		}
+
+		int findPos(int[] a, int key, int lo, int hi) {
+			while (hi >= lo) {
+				int mid = lo + (hi - lo) / 2;
+				if (key < a[mid]) {
+					hi = mid - 1;
+				} else if (key > a[mid])
+					lo = mid + 1;
+				else
+					return mid;
+			}
+			return lo;
+		}
+
+		int get(int key) {
+			int lo = 0, hi = cnt - 1;
+			while (hi >= lo) {
+				int mid = lo + (hi - lo) / 2;
+				if (key < keys[mid])
+					hi = mid - 1;
+				else if (key > keys[mid])
+					lo = mid + 1;
+				else
+					return values[mid];
+			}
+			return -1;
+		}
+
+		void swap(int[] a, int i, int j) {
+			int tmp = a[i];
+			a[i] = a[j];
+			a[j] = tmp;
+		}
+
+		int[] keys;
+		int[] values;
+		int cnt = 0;
+		int capacity;
+	}
+
+	public int[] twoSum(int[] numbers, int target) {
+		int[] ret = null;
+		int len = numbers.length;
+		SortedTable st = new SortedTable( len );
+		for( int i=0;i<len;i++ ){
+			st.put(numbers[i], i+1);
+		}
+		for( int j=0;j<len;j++ ){
+			int idx = st.get(target-numbers[j]);
+			if( idx!=-1&& idx>j  ){
+				ret = new int[2];ret[0]=j+1;ret[1]=idx;
+			}
+		}
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {2,7,11,5};
+		System.out.println( Arrays.toString( new TwoSum().twoSum(a, 9)  ) );
+	}
 }
