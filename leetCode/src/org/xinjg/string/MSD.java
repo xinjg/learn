7198c1009f681bf2b3ba7cfb356c991be904bbae
xinjg <574218911@qq.com>
2015-05-22 13:22:51 +0800

    Refractor class name

--- leetCode/src/org/xinjg/string/MSD.java
+++ leetCode/src/org/xinjg/string/MSD.java
@@ -2,7 +2,7 @@ package org.xinjg.string;
 
 import java.util.Arrays;
 
-public class WSD {
+public class MSD {
 	public static int charAt(String string ,int pos) {
 		if (pos>=string.length()) {
 			return  -1;
@@ -19,18 +19,23 @@ public class WSD {
 		if (lo>=hi) {
 			return;
 		}
+		/* Key indexed counting */
 		int R=256;
 		int[] count=new int[R+2];
+		//count frequency
 		for (int i = 0; i < array.length; i++) {
 			count[ charAt(array[i], d)+2 ]++;
 		}
+		//convert frequency to index
 		for (int i = 1; i < count.length; i++) {
 			count[i]=count[i-1]+count[i];
 		}
+		// move array items to aux
 		String[] aux =new String[array.length];
 		for (int i = 0; i < array.length; i++) {
 			aux[ count[charAt(array[i], d)+1] ++]=array[i];
 		}
+		//copy back
 		for (int i = 0; i < aux.length; i++) {
 			array[i]=aux[i];
 		}
